09ffc7617c8fe3f72daceb949c2ae1dcf5aec05f
Fernando Torres <fernan.orres@gmail.com>
2011-06-19 17:20:46 +0200

    aefzez

--- application/MovingShapes.java
+++ application/MovingShapes.java
@@ -1,20 +1,103 @@
 package application;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
+import java.awt.Color;
 
-import compositor.Compositor;
 
-public class MovingShapes extends Thread implements Application{
+public MovingShapes() {
+	start();
+}
 
-	private Integer x=0,y=0,w=0,h=0;
+public void draw(Graphics2D context, int parX, int parY, int parW, int parH) {
+	x=parX; y=parY; w=parW; h=parH;
 	
-	public void draw(Graphics2D context, int parX, int parY, int parW, int parH) {
+	context.setColor(couleur);
+	context.fillRect(x, y, w, h);
+}
 
-		
-		context.fillRect(x, y, w, h);
+	
+
+	 
+
+
+	  public void updateLocation(MouseEvent e) {
+	    rect.setLocation(preX + e.getX(), preY + e.getY());
+
+	    if (checkRect()) {
+	      ShapeMover.label.setText(rect.getX() + ", " + rect.getY());
+	    } else {
+	      ShapeMover.label.setText("drag inside the area.");
+	    }
+
+	    repaint();
+	  }
+
+	  public void paint(Graphics g) {
+	    update(g);
+	  }
+
+	  public void update(Graphics g) {
+	    Graphics2D g2 = (Graphics2D) g;
+	    Dimension dim = getSize();
+	    int w = (int) dim.getWidth();
+	    int h = (int) dim.getHeight();
+	    g2.setStroke(new BasicStroke(8.0f));
+
+	    if (isFirstTime) {
+	      area = new Rectangle(dim);
+	      rect.setLocation(w / 2 - 50, h / 2 - 25);
+	      isFirstTime = false;
+	    }
+
+	    // Clears the rectangle that was previously drawn.
+	    g2.setPaint(Color.white);
+	    g2.fillRect(0, 0, w, h);
+
+	    g2.setColor(Color.red);
+	    g2.draw(rect);
+	    g2.setColor(Color.black);
+	    g2.fill(rect);
+	  }
+
+	  boolean checkRect() {
+	    if (area == null) {
+	      return false;
+	    }
+
+	    if (area.contains(rect.x, rect.y, 100, 50)) {
+	      return true;
+	    }
+	    int new_x = rect.x;
+	    int new_y = rect.y;
+
+	    if ((rect.x + 100) > area.getWidth()) {
+	      new_x = (int) area.getWidth() - 99;
+	    }
+	    if (rect.x < 0) {
+	      new_x = -1;
+	    }
+	    if ((rect.y + 50) > area.getHeight()) {
+	      new_y = (int) area.getHeight() - 49;
+	    }
+	    if (rect.y < 0) {
+	      new_y = -1;
+	    }
+	    rect.setLocation(new_x, new_y);
+	    return false;
+	  }
 	}
+	           
+	         
+	    
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	@Override
 	public void run() {
--- compositor/Compositor.java
+++ compositor/Compositor.java
@@ -22,6 +22,7 @@ import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 
+import application.BouncingPoint;
 import application.SmoothChange;
 
 import window.Window;
@@ -71,7 +72,7 @@ public class Compositor extends JFrame implements MouseListener, MouseMotionList
 		// le nombre max d'icones sur une hauteur de fenetre
 		
 		for(int i=0;i<10;i++) {
-			windows.add(new Window(new SmoothChange(), 110*(1+(i%5)), 110*(1+(i/5))));
+			windows.add(new Window(new BouncingPoint(), 110*(1+(i%5)), 110*(1+(i/5))));
 		} // on ajoute 10 applications
 		
 		addMouseListener(this);
